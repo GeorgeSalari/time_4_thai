e77dec776dfd9833763323c5ff3996bd7c07ee78
GeorgeSalari <x3m.giku@gmail.com>
2018-11-16 17:09:00 +0700

    show in welcome page first 6 tours

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,6 @@ class WelcomeController < ApplicationController
   before_action :new_order, :new_call_order, only: :index
 
   def index
-    @sea_tours = SeaTour.all
+    @sea_tours = SeaTour.all.first(6)
   end
 end
