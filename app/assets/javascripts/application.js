b2c538b62580f2084f7cbd256ce33d4b1dcd73da
GeorgeSalari <x3m.giku@gmail.com>
2018-12-20 23:50:22 +0700

    fixed tiger price and individual tours price

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,7 +18,7 @@
 //= require_tree .
 
 $(document).ready(function(){
-  if (window.location.href.includes('boats') || window.location.href.includes('sea_tours/10') || window.location.href.includes('realty-buy') || window.location.href.includes('transfers') || window.location.href.includes('photoshoot') || window.location.href.includes('wedding') || window.location.href.includes('spa') ) {
+  if (window.location.href.includes('boats') || window.location.href.includes('sea_tours/10') || window.location.href.includes('realty-buy') || window.location.href.includes('transfers') || window.location.href.includes('photoshoot') || window.location.href.includes('wedding') || window.location.href.includes('spa') || window.location.href.includes('individual')|| window.location.href.includes('phuket_tours/4')  ) {
     $($('.price-container').children()[0]).find('p').text('От '+$($('.price-container').children()[0]).find('p').text().trim().split(' ')[1] + " THB");
     $($('.price-container').children()[1]).find('p').hide();
     $($('.price-container').children()[2]).find('button').css('right', '0');
