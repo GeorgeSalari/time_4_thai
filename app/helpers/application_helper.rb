7f120e0d04f018a6332d6568a35c17a4d0f58e27
GeorgeSalari <x3m.giku@gmail.com>
2019-02-10 17:11:46 +0700

    fixed view of comments in main page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -77,4 +77,26 @@ module ApplicationHelper
     all_index_tour = shops + boats + sea_tours + land_tours + evening_shows + phuket_tours + avia_tours + individual_tours
     all_index_tour
   end
+
+  def find_item_tour(product_id, product_type)
+    case product_type
+    when 'SeaTour'
+      SeaTour.find(product_id)
+    when 'LandTour'
+      LandTour.find(product_id)
+    when 'EveningShow'
+      EveningShow.find(product_id)
+    when 'PhuketTour'
+      PhuketTour.find(product_id)
+    when 'AviaTour'
+      AviaTour.find(product_id)
+    when 'Boat'
+      Boat.find(product_id)
+    when 'Shop'
+      Shop.find(product_id)
+    when 'StaticPage'
+      StaticPage.find(product_id)
+    end
+  end
+  
 end
